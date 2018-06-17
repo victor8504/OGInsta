6e1b875429763c0a2f11751a9a5ff480f5c6b059
victor njuguna <victormoore254@gmail.com>
2018-06-17 22:22:47 +0300

    this commit defines the logout url pattern

--- gram/urls.py
+++ gram/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('oginsta.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/accounts/login/'}), 
 ]
