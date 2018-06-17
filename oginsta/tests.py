55069173149c7275313debca912629007fee4111
victor njuguna <victormoore254@gmail.com>
2018-06-17 16:48:31 +0300

    this commit creates a test_instance for the Image model and make migrations

--- oginsta/tests.py
+++ oginsta/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Profile, Image, Comment, Like
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.image = Image(name = 'Sunset', caption = 'Beach sunset in Thailand')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
