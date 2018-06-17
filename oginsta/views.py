5c25946c77fefc4d6373066471964eab178ead22
victor njuguna <victormoore254@gmail.com>
2018-06-17 22:15:33 +0300

    this commit implements user authentication int the whole project starting with the home page

--- oginsta/views.py
+++ oginsta/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from .models import Profile, Image, Comment, Like
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def home(request):
     images = Image.get_images()
     return render(request, 'home.html', {"images":images})
