76eafaece3211e7386a7ca588e6670f589a9d660
victor njuguna <victormoore254@gmail.com>
2018-06-17 19:08:24 +0300

    this commit creates the home url

--- oginsta/views.py
+++ oginsta/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Profile, Image, Comment, Like
 
 # Create your views here.
+def home(request):
+    images = Image.get_images()
+    return render(request, 'home.html', {"images":images})
