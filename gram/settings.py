8860b82bef9b81fa6ca61cb7eafe0f0909c4d34d
victor njuguna <victormoore254@gmail.com>
2018-06-17 17:16:56 +0300

    this commit sets up the django registration urls and settings for use of confirmation email

--- gram/settings.py
+++ gram/settings.py
@@ -138,3 +138,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value
