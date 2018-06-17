88f32f43856c65892d74deb1b44d3bd2752f466b
victor njuguna <victormoore254@gmail.com>
2018-06-17 15:49:33 +0300

    this commit creates the profile model

--- oginsta/models.py
+++ oginsta/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to = 'profile_pic/', null = True)
+    bio = models.TextField(max_length = 50, blank = True)
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+
+
+
+
+
