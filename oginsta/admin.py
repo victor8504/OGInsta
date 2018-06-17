43bd51132a35bf9f2e374c2c822283e408ac3afc
victor njuguna <victormoore254@gmail.com>
2018-06-17 17:06:56 +0300

    this commit adds the models to the admin page

--- oginsta/admin.py
+++ oginsta/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile, Image, Comment, Like
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Like)
+
