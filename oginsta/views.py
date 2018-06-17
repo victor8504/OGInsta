366e72ad11517a8f4a4e268734599e16e71b9404
victor njuguna <victormoore254@gmail.com>
2018-06-17 23:12:44 +0300

    this commit creates the post view function

--- oginsta/views.py
+++ oginsta/views.py
@@ -13,8 +13,28 @@ def home(request):
 
 @login_required(login_url='/accounts/login/')
 def profile(request):
-    user = User.objects.get(username = username)
+    current_user = request.user
     images = Image.objects.filter(user_id = user)
-    profile = Profile.objects.get(user=user)
+    profile = Profile.objects.get(user=request.user)
 
-    return render(request, 'profile.html', {"user": user,"images": images,"profile": profile})
+    return render(request, 'profile.html', {"current_user": current_user,"images": images,"profile": profile})
+
+
+@login_required(login_url='/accounts/login/')
+def post(request):
+    current_user = request.user
+    profiles = Profile.objects.all()
+    for profile in profiles:
+        if profile.user.id == current_user.id:
+            if request.method == 'POST':
+                form = PostForm(request.POST, request.FILES)
+                if form.is_valid():
+                    post = form.save(commit=False)
+                    post.name = current_user
+                    post.profile = profile
+                    post.save()
+                    return redirect('home')
+            else:
+                form = PostForm()
+
+    return render(request, 'post.html', {"post_form": form,"user": current_user})
